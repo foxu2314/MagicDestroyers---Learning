1baa9ea2bc9eae6ac4fdd095944dd8c38e55b59f
Adrian Skiba <meanduk@gmail.com>
2019-02-12 22:25:14 +0100

    Add project files.

---
+++ CSharpOOP/Characters/Warriors/Warrior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Characters.Warriors
+{
+    public class Warrior
+    {
+        private int height;
+        private int weight;
+        private string name;
+
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public string Name { get; set; }
+
+        public Warrior(int height, int weight, string name)
+        {
+            this.Height = height;
+            this.Weight = weight;
+            this.Name = name;
+        }
+
+        public void Greetings(Warrior warrior)
+        {
+            Console.WriteLine($"Greetings {warrior.Name}!");
+        }
+    }
+}
---
+++ CSharpOOP/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using Characters.Warriors;
+using System;
+
+public class EntryPoint
+{
+    static void Main()
+    {
+        Warrior theGoodGuy = new Warrior(190, 80, "Good Guy");
+        Warrior theBadGuy = new Warrior(170, 80, "Bad Guy");
+
+        Console.WriteLine(theBadGuy.Height);
+        Console.WriteLine(theGoodGuy.Height);
+
+        theGoodGuy.Greetings(theBadGuy);
+        theBadGuy.Greetings(theGoodGuy);
+    }
+}
