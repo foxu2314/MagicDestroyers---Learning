eb0142f0ba4906bcc93eb4d70228d63385969823
Adrian Skiba <meanduk@gmail.com>
2019-02-12 23:11:26 +0100

    Walidacja oraz exception. Czas teraz na 2 projekt

--- CSharpOOP/Characters/Warriors/Warrior.cs
+++ CSharpOOP/Characters/Warriors/Warrior.cs
@@ -6,11 +6,53 @@ namespace Characters.Warriors
     {
         private int height;
         private int weight;
+        private int age;
         private string name;
 
-        public int Height { get; set; }
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value>=18 && value<=45)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, $@"The selected age for {name} is not a proper for a warrior. The age must be between 18 and 45");
+                }
+                
+            }
+        }
         public int Weight { get; set; }
-        public string Name { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
 
         public Warrior(int height, int weight, string name)
         {
--- CSharpOOP/EntryPoint.cs
+++ CSharpOOP/EntryPoint.cs
@@ -8,10 +8,28 @@ public class EntryPoint
         Warrior theGoodGuy = new Warrior(190, 80, "Good Guy");
         Warrior theBadGuy = new Warrior(170, 80, "Bad Guy");
 
-        Console.WriteLine(theBadGuy.Height);
-        Console.WriteLine(theGoodGuy.Height);
+        //Console.WriteLine(theBadGuy.Height);
+        //Console.WriteLine(theGoodGuy.Height);
 
-        theGoodGuy.Greetings(theBadGuy);
-        theBadGuy.Greetings(theGoodGuy);
+        
+
+        Console.WriteLine(theGoodGuy.Age);
+        Console.WriteLine(theBadGuy.Age);
+
+        try
+        {
+            theGoodGuy.Age = 25;
+            theBadGuy.Age = 15;
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+
+        //theGoodGuy.Greetings(theBadGuy);
+        //theBadGuy.Greetings(theGoodGuy);
+
+        Console.ReadKey();
     }
 }
