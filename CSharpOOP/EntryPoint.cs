381abaec9cb507dc21aec7c2e9c49409d63638ce
4rczik <4rczik@gmail.com>
2019-02-17 11:43:56 +0100

    refactoring,komentarze etc

--- CSharpOOP/EntryPoint.cs
+++ CSharpOOP/EntryPoint.cs
@@ -1,4 +1,5 @@
 ﻿using Characters.Warriors;
+using Weapons;
 using System;
 
 public class EntryPoint
@@ -8,6 +9,23 @@ public class EntryPoint
         Warrior theGoodGuy = new Warrior(190, 80, "Good Guy");
         Warrior theBadGuy = new Warrior(170, 80, "Bad Guy");
 
+        Warrior theTestGuyWhoLooksLikeTotallyBadass = new Warrior(100, 150, "BadAss 4rcz!k");
+        Warrior theTestGuyWhoLooksLikeTotallyBadassTwin = new Warrior(101, 149, "BadAss 4rcz!k evil twin", 76, new Sword(29));
+
+        //wojownik z deafultowymi wartosciami
+        theTestGuyWhoLooksLikeTotallyBadass.WhoAmI();
+        theTestGuyWhoLooksLikeTotallyBadass.HowMuchDemageCanICause();
+        theTestGuyWhoLooksLikeTotallyBadass.AbilityOfMySwordToKillDragons();
+
+        //wojownik z podanymi wartosciami
+        theTestGuyWhoLooksLikeTotallyBadassTwin.WhoAmI();
+        theTestGuyWhoLooksLikeTotallyBadassTwin.HowMuchDemageCanICause();
+        theTestGuyWhoLooksLikeTotallyBadassTwin.AbilityOfMySwordToKillDragons();
+
+
+
+
+        //Console.WriteLine(theGoodGuy.SwordWeapon.Damage);
         //Console.WriteLine(theBadGuy.Height);
         //Console.WriteLine(theGoodGuy.Height);
 
@@ -30,9 +48,9 @@ public class EntryPoint
         //theGoodGuy.Greetings("Tod");
         //theBadGuy.Greetings(theGoodGuy);
 
-        Console.WriteLine(theGoodGuy.SwordWeapon.Damage);
+        //Console.WriteLine(theGoodGuy.SwordWeapon.Damage);
 
-        Console.ReadKey();
+        //Console.ReadKey();
     }
     
 }
--- CSharpOOP/Weapons/Sword.cs
+++ CSharpOOP/Weapons/Sword.cs
@@ -5,6 +5,16 @@ namespace Weapons
     {
         private int damage;
 
+        protected int damagaeNew=69; //nowe pole ktore ma access modifier 'protected', tzn ze masz do neigo dostep
+                                     //w tej klasie(Sword) oraz w Klasie ktora dziedziczy po klasie Sword czyli Warrior
+                                     //inicjalizujemy wartoscia 69
+
+        //note that: od c# 6.0(od viual studio chyba 2015) mozesz inicjalizowac properties, 
+        //w poprzednich wersjach mozesz to zrobic dopiero w konstruktorze np:
+        public int MyProperty1 { get; set; } = 80;
+
+
+
         public int Damage
         {
             get
@@ -20,7 +30,11 @@ namespace Weapons
 
         public Sword()
         {
-            this.Damage = 10;
+            this.Damage = 10; //jak nie podamy przy kontrukcji miecza to deafultowo 10
+        }
+        public Sword(int _sword)//2 konstruktor z parametrem, jka podamy to wartosc
+        {
+            this.Damage = _sword;
         }
 
     }
